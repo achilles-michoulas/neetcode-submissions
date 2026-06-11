1d5c96879f3e1315f60e86b688f93033f4d3d805
Achilles Michoulas <achilles.michoulas@icloud.com>
2026-06-11 10:39:24 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        
+        for string in strs:
+            charCounts = {}
+            
+            for char in string:
+                charCounts[char] = charCounts.get(char, 0) + 1
+
+            key = tuple(sorted(charCounts.items()))
+
+            if key in groups:
+                groups[key].append(string)
+            else:
+                groups[key] = [string]
+
+        return list(groups.values())
+
+        
+            
+
+
+        
